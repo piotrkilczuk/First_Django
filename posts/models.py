abe4eb74bca40b8a2040bcd14e1195c7b61ff797
Piotr Pawełczak <pawelczak.piotr@gmail.com>
2021-04-15 12:02:51 +0200

    Fixed posts.models

--- posts/models.py
+++ posts/models.py
@@ -1,23 +1,22 @@
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=20)
-    content = models.TextField(max_length=200)
+    title = models.CharField(max_length=35)
+    content = models.TextField(max_length=500)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
-    author_id = models.ForeignKey(
+    author = models.ForeignKey(
         'posts.Author',
         on_delete=models.CASCADE,
-        null=True,
-        blank=True
+        blank=False
     )
 
     def __str__(self):
         return f" title: {self.title}, content: {self.content[:30]} author: {self.author_id}"
 
 class Author(models.Model):
-    nick = models.CharField(max_length=20)
-    email = models.EmailField(max_length=40)
+    nick = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return f"nick: {self.nick}, email: {self.email}"
